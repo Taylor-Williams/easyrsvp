382df26525354d9d2e66fefb8020b937b8778ca8
Taylor-Williams <taylor@thewilliams.net>
2021-06-24 14:16:25 -0700

    build out basic venues, reservations controllers

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::ReservationsController < ApplicationController
+
+    def index
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def reservation_params
+        params.require(:reservation).permit(:venue_id, :date, :res_name, :phone_number, :res_size)
+    end
+
 end
--- app/controllers/api/v1/venues_controller.rb
+++ app/controllers/api/v1/venues_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::VenuesController < ApplicationController
+
+    def index
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def reservation_params
+        params.require(:venue).permit(:name, :occupany, :location)
+    end
+
 end
