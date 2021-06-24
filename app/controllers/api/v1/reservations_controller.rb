e19cb2a967e2ab7751958c48c2e10391ee57e401
Taylor-Williams <taylor@thewilliams.net>
2021-06-24 14:56:50 -0700

    add initial reservations controller logic

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,22 +1,37 @@
 class Api::V1::ReservationsController < ApplicationController
 
-    def index
+    before_action :set_venue
 
+    def index
+        @reservations = @venue.reservations
+        render json: @reservations
     end
 
     def create
-
+        @reservation = Reservation.new(reservation_params)
+        #TODO: build out logic to only allow certain # of reservations for time period
+        if @reservation.save
+            render json: @reservation
+        else
+            render json: {error: "error creating reservation"}
+        end
     end
 
     def show
-
+        @reservation = Reservation.find(params[:id])
+        render json: @reservation
     end
 
     def destroy
-
+        @reservation = Reservation.find(params[:id])
+        @reservation.destroy
     end
 
     private
+
+    def set_venue
+        @venue = Venue.find(params[:venue_id])
+    end
 
     def reservation_params
         params.require(:reservation).permit(:venue_id, :date, :res_name, :phone_number, :res_size)
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :reservations
-      resources :venues
+      resources :venues do
+        resources :reservations
+      end
     end
   end
 end
