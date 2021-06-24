fb3e197187bc9793d4ab9a8097543cc5ae57fdce
Taylor-Williams <taylor@thewilliams.net>
2021-06-24 12:12:47 -0700

    add venue, reservations resources

---
+++ db/migrate/20210624190448_create_venues.rb
@@ -0,0 +1,11 @@
+class CreateVenues < ActiveRecord::Migration[6.1]
+  def change
+    create_table :venues do |t|
+      t.string :name
+      t.integer :occupancy
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
