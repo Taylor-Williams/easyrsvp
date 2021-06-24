fa45b734e388365b2e65d04c091609d63c4602fc
Taylor-Williams <taylor@thewilliams.net>
2021-06-24 14:26:02 -0700

    add index, show, create, destroy logic to venue controller

--- app/controllers/api/v1/venues_controller.rb
+++ app/controllers/api/v1/venues_controller.rb
@@ -1,19 +1,27 @@
 class Api::V1::VenuesController < ApplicationController
 
     def index
-
+        @venues = Venue.all
+        render json: @venues
     end
 
     def create
-
+        @venue = Venue.new(venue_params)
+        if @venue.save
+            render json: @venue
+        else
+            render json: {error: "error creating venue"}
+        end
     end
 
     def show
-
+        @venue = Venue.find(params[:id])
+        render json: @venue
     end
 
     def destroy
-
+        @venue = Venue.find(params[:id])
+        @venue.destroy
     end
 
     private
