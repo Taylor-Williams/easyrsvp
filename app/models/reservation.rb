dd75c69dfa591a3ba420bc31c275dfbf4e214b6a
Taylor-Williams <taylor@thewilliams.net>
2021-06-24 12:19:52 -0700

    add venue, reservations relationships and validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,4 @@
 class Reservation < ApplicationRecord
+    belongs_to :venue
+    validates :res_name, :phone_number, :res_size, presence: true
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,4 @@
 class Venue < ApplicationRecord
+    has_many :reservations
+    validates :name, :occupancy, :location, presence: true
 end
